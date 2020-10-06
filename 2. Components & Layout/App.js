d23d08a53ae1a223fc987daa2e979e80aedb9ea3
JuliWolf <qwert0038@yandex.ru>
2020-10-06 19:01:30 +0300

    GameScreen

--- 2. Components & Layout/App.js
+++ 2. Components & Layout/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from "./components/Header";
-import StartGamScreen from "./screens/StartGameScreen";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.screen}>
-      <Header title="Guess a Number"/>
-      <StartGamScreen/>
-    </View>
-  );
+    const [userNumber, setUserUmber] = useState();
+
+    const startGameHandler = selectedNumber => {
+        setUserUmber(selectedNumber);
+    };
+
+    let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+    if(userNumber){
+        content = <GameScreen userChoice={userNumber}/>
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Header title="Guess a Number"/>
+            {content}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
+    screen: {
+        flex: 1
+    }
 });
--- 2. Components & Layout/screens/StartGameScreen.js
+++ 2. Components & Layout/screens/StartGameScreen.js
@@ -15,7 +15,7 @@ import NumberContainer from "../components/NumberContainer";
 
 import Colors from '../constants/color';
 
-const StartGamScreen = (props) => {
+const StartGameScreen = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
     const [confirmed, setConfirmed] = useState(false);
     const [selectedNumber, setSelectedNumber] = useState();
@@ -52,7 +52,7 @@ const StartGamScreen = (props) => {
                 <NumberContainer>
                     {selectedNumber}
                 </NumberContainer>
-                <Button title='START GAME'/>
+                <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)}/>
             </Card>
         )
     }
@@ -132,4 +132,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default StartGamScreen;
+export default StartGameScreen;

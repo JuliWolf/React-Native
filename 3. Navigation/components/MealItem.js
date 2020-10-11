7122caf75fc757da23ac5a7235d8b3638d384f10
JuliWolf <qwert0038@yandex.ru>
2020-10-11 22:03:41 +0300

    use custom fonts

--- 3. Navigation/components/MealItem.js
+++ 3. Navigation/components/MealItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet, ImageBackground} from "react-native";
 
 import TouchableCmp from './TouchableCmp';
+import DefaultText from './DefaultText';
 
 const MealItem = (props) => {
     return (
@@ -11,14 +12,14 @@ const MealItem = (props) => {
                     <View style={{...styles.mealRow, ...styles.mealHeader}}>
                         <ImageBackground source={{uri: props.image}} style={styles.bgImage}>
                             <View style={styles.titleContainer}>
-                                <Text style={styles.title} numberOfLines={1}>{props.title}</Text>
+                                <DefaultText style={styles.title} numberOfLines={1}>{props.title}</DefaultText>
                             </View>
                         </ImageBackground>
                     </View>
                     <View style={{...styles.mealRow, ...styles.mealDetail}}>
-                        <Text>{props.duration}m</Text>
-                        <Text>{props.complexity.toUpperCase()}</Text>
-                        <Text>{props.affordability.toUpperCase()}</Text>
+                        <DefaultText>{props.duration}m</DefaultText>
+                        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+                        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
                     </View>
                 </View>
             </TouchableCmp>

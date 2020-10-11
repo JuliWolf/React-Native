266bd57966ad593e78ea7d950db28384416533e9
JuliWolf <qwert0038@yandex.ru>
2020-10-11 22:00:28 +0300

    use custom fonts

--- 3. Navigation/navigation/MealsNavigator.js
+++ 3. Navigation/navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Platform} from "react-native";
+import {Platform, Text} from "react-native";
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -18,6 +18,12 @@ import Colors from "../constants/Colors";
 const defaultStackNavOptions = {
     headerStyle: {
         backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans'
     },
     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
@@ -52,17 +58,19 @@ const tabScreenConfig = {
                 return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>
             }
         },
-        tabBarColor: Colors.primaryColor
+        tabBarColor: Colors.primaryColor,
+        tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Meals</Text> : 'Meals'
     },
     Favorites: {
         screen: FavoritesNavigator,
         navigationOptions: {
-            tabBarLabel: 'Favourites!',
+            // tabBarLabel: 'Favourites!',
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>
             },
             tabBarColor: Colors.accentColor
-        }
+        },
+        tabBarLabel: Platform.OS === 'android' ? <Text style={{fontFamily: 'open-sans-bold'}}>Favourites!</Text> : 'Favourites!'
     }
 };
 
@@ -77,6 +85,9 @@ const MealsFavTabNavigator = Platform.OS === 'android' ?
     }) :
     createBottomTabNavigator(tabScreenConfig, {
         tabBarOptions: {
+            labelStyle: {
+                fontFamily: 'open-sans-bold'
+            },
             activeTintColor: Colors.accentColor
         }
 });

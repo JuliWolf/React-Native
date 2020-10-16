34dc4c23115b245796b989e70965e4614e9080ff
JuliWolf <qwert0038@yandex.ru>
2020-10-16 18:53:42 +0300

    connect redux

--- 3. Navigation/App.js
+++ 3. Navigation/App.js
@@ -3,11 +3,19 @@ import { StyleSheet } from 'react-native';
 import * as Font from 'expo-font';
 import {AppLoading} from "expo";
 import {enableScreens} from "react-native-screens";
+import { createStore, combineReducers} from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealReducer from "./store/reducers/meals";
 
 // More performing
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealReducer
+});
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -27,7 +35,9 @@ export default function App() {
     />
   }
 
-  return <MealsNavigator/>;
+  return <Provider store={store}>
+            <MealsNavigator/>
+         </Provider>;
 }
 
 

a3db66abbd85cdc220e2c87e59790843ad8ce31d
JuliWolf <qwert0038@yandex.ru>
2020-10-16 19:31:03 +0300

    Add to favourite

--- 3. Navigation/store/reducers/meals.js
+++ 3. Navigation/store/reducers/meals.js
@@ -1,3 +1,4 @@
+import * as mealActions from '../actions/meals';
 import {MEALS} from "../../data/dummy-data";
 
 const initialState = {
@@ -7,7 +8,28 @@ const initialState = {
 }
 
 const mealReducer = (state = initialState, action) => {
+    switch(action.type){
+        case mealActions.TOGGLE_FAVORITE:
+            return toggleFavoriteHandler(state, action);
+        default:
+            return state;
+    }
     return state;
+}
+
+function toggleFavoriteHandler(state, action){
+    const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
+    if(existingIndex > 0){
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return {...state, favoriteMeals: updatedFavMeals}
+    }else{
+        return {...state, favoriteMeals: state.favoriteMeals.concat(
+            state.meals.find(meal =>
+                meal.id === action.mealId
+            ))
+        }
+    }
 }
 
 export default mealReducer;

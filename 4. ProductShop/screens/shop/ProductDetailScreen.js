22140798197e858e61afa7ce437a9b54a6da29ea
JuliWolf <qwert0038@yandex.ru>
2020-10-17 19:01:37 +0300

    navigate to productDetail screen

--- 4. ProductShop/screens/shop/ProductDetailScreen.js
+++ 4. ProductShop/screens/shop/ProductDetailScreen.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, ScrollView, Image, Button} from "react-native";
+import {useSelector} from "react-redux";
 
 const ProductDetailScreen = (props) => {
+    const productId = props.navigation.getParam('productId');
+    const selectedProduct = useSelector(state => state.products.availableProducts.find(product => product.id === productId));
+    return (
+        <ScrollView>
+            <View>
+                <Text>{selectedProduct.title}</Text>
+            </View>
+        </ScrollView>
+    );
+};
 
+ProductDetailScreen.navigationOptions = navData => {
+    return {
+        headerTitle: navData.navigation.getParam('productTitle')
+    }
 };
 
 const styles = StyleSheet.create({

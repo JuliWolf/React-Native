b9287d73982ca96bc6c9ee427aff5514444658fc
JuliWolf <qwert0038@yandex.ru>
2020-10-18 11:04:23 +0300

    styling Order Item show and hide details button

--- 4. ProductShop/components/shop/OrderItem.js
+++ 4. ProductShop/components/shop/OrderItem.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from "react-native";
 
 import Colors from "../../constants/Colors";
+import CartItem from "./CartItem";
 
 const OrderItem = (props) => {
+    const [showDetails, setShowDetails] = useState(false);
+
     return (
         <View style={styles.orderItem}>
             <View style={styles.summary}>
                 <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
                 <Text style={styles.date}>{props.date}</Text>
             </View>
-            <Button title="show Details" color={Colors.primary}/>
+            <Button
+                title={showDetails ? 'Hide Details' :"Show Details"}
+                color={Colors.primary}
+                onPress={() => {
+                setShowDetails(prevState => !prevState);
+            }}/>
+            {showDetails && <View style={styles.detailItems}>
+                {props.items.map(cartItem =>
+                    <CartItem
+                        quantity={cartItem.quantity}
+                        title={cartItem.productTitle}
+                        amount={cartItem.sum}
+                        deletable={false}
+                        key={cartItem.productId}
+                    />
+                    )
+                }
+            </View>}
         </View>
     );
 };
@@ -46,6 +66,9 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans',
         fontSize: 16,
         color: '#888'
+    },
+    detailItems: {
+        width: '100%'
     }
 });
 

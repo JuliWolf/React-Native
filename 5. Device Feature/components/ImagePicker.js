a1c46fca125fb0405663f3a07153887e11c1a500
JuliWolf <qwert0038@yandex.ru>
2020-10-20 19:46:27 +0300

    add permission

--- 5. Device Feature/components/ImagePicker.js
+++ 5. Device Feature/components/ImagePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, Image} from "react-native";
+import {View, Text, StyleSheet, Button, Image, Alert} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 import Colors from "../constants/Colors";
 
 const ImgPicker = (props) => {
-    const takeImageHandler = () => {
+    const verifyPermissions = async () => {
+        const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.CAMERA);
+        if(result.status !== 'granted'){
+            Alert.alert('Insufficient permissions!', 'You need to grant camera permissions to use this app.', [{
+                text: 'Okay'
+            }]);
+            return false;
+        }
+        return true;
+    };
+
+    const takeImageHandler = async() => {
+        const hasPermission = await verifyPermissions();
+        if(!hasPermission)
+            return;
         ImagePicker.launchCameraAsync();
     };
 

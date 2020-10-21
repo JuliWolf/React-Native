a1616a5557ad0c1b59c4b3ff9bb4c6d258e8b198
JuliWolf <qwert0038@yandex.ru>
2020-10-21 22:33:48 +0300

    fix

---
+++ 5. Device Features/screens/NewPlaceScreen.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import { View, Text, StyleSheet, TextInput, ScrollView, Button } from 'react-native';
+
+import ImagePicker from '../components/ImagePicker';
+
+import * as placesActions from '../store/places-actions';
+
+import Colors from "../constants/Colors";
+
+const NewPlaceScreen = props => {
+    const [titleValue, setTitleValue] = useState('');
+    const [imageValue, setImageValue] = useState();
+
+    const dispatch = useDispatch();
+
+    const titleChangeHandler = text => {
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        dispatch(placesActions.addPlace(titleValue, imageValue));
+        props.navigation.goBack();
+    };
+
+    const imageTakenHandler = (imagePath) => {
+        setImageValue(imagePath);
+    };
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue}/>
+                <ImagePicker onImageTaken={imageTakenHandler}/>
+                <Button title='Save Place' color={Colors.primary} onPress={savePlaceHandler}/>
+            </View>
+        </ScrollView>
+    );
+};
+
+NewPlaceScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Add Place'
+    }
+}
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 30,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
+
+export default NewPlaceScreen;

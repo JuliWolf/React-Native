80e6f5fedc5076756282738c9c904eda66a35f93
JuliWolf <qwert0038@yandex.ru>
2020-10-24 14:27:00 +0300

    configure main navigation component

--- 6. Switch to new Navigation/navigation/AppNavigator.js
+++ 6. Switch to new Navigation/navigation/AppNavigator.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import {NavigationContainer} from "@react-navigation/native";
-import {  }
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import ShopNavigator from "./ShopNavigator";
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+
+const myStack = createStackNavigator();
 
 const AppNavigation = (props) => {
     const isAuth = useSelector(state => !!state.auth.token);
 
-    return ;
+    return (
+        <NavigationContainer>
+            <MyStack.Navigator>
+                <MyStack.Screen
+                    name="ProductsOverview"
+                    component={ProductsOverviewScreen}
+                />
+            </MyStack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default AppNavigation;

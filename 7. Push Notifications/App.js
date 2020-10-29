1247d9aa231d4f5e692d132b0bc6fe1fabf5ddde
JuliWolf <qwert0038@yandex.ru>
2020-10-29 21:32:49 +0300

    sending notification via the app

--- 7. Push Notifications/App.js
+++ 7. Push Notifications/App.js
@@ -1,5 +1,4 @@
-import {StatusBar} from 'expo-status-bar';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Button} from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
@@ -13,6 +12,7 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+    const [pushToken, setPushToken] = useState('');
     useEffect(() => {
         Permissions.getAsync(Permissions.NOTIFICATIONS)
             .then(statusObj => {
@@ -31,8 +31,7 @@ export default function App() {
             })
             .then(response => {
                 const token = response.data;
-                console.log(response);
-
+                setPushToken(token);
             })
             .catch((err) => {
                 return null;
@@ -66,11 +65,26 @@ export default function App() {
         //     }
         // });
 
+        fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Accept-Encoding': 'gzip, deflate',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                to: pushToken,
+                data: {
+                    extraData: 'Some data',
+                },
+                title: 'Sent via the app',
+                body: 'This push was sent via the app!'
+            })
+        });
     };
     return (
         <View style={styles.container}>
             <Button title="Trigger notification" onPress={triggerNotificationHandler}/>
-            <StatusBar style="auto"/>
         </View>
     );
 }
